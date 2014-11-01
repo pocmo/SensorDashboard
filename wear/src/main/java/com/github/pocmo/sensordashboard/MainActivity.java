712c866238c522800e16f68200da3c6b804b316f
Benjamin Stürmer <benjamin.stuermer@quartett-mobile.de>
2014-11-01 20:05:59 +0000

    Add more sensors

--- wear/src/main/java/com/github/pocmo/sensordashboard/MainActivity.java
+++ wear/src/main/java/com/github/pocmo/sensordashboard/MainActivity.java
@@ -39,19 +39,29 @@ public class MainActivity extends Activity implements SensorEventListener {
     private final static int SENS_STEP_COUNTER = Sensor.TYPE_STEP_COUNTER;
     private final static int SENS_STEP_DETECTOR = Sensor.TYPE_STEP_DETECTOR;
 
-
     SensorManager mSensorManager;
 
-    private Sensor mGameRotationVectorSensor;
-    private Sensor mAmbientTemperatureSensor;
     private Sensor mAccelerometerSensor;
+    private Sensor mAmbientTemperatureSensor;
+    private Sensor mGameRotationVectorSensor;
     private Sensor mGeomagneticSensor;
     private Sensor mGravitySensor;
     private Sensor mGyroscopeSensor;
     private Sensor mGyroscopeUncalibratedSensor;
     private Sensor mHeartrateSensor;
+    private Sensor mLightSensor;
+    private Sensor mLinearAccelerationSensor;
+    private Sensor mMagneticFieldSensor;
+    private Sensor mMagneticFieldUncalibratedSensor;
+    private Sensor mPressureSensor;
+    private Sensor mProximitySensor;
+    private Sensor mHumiditySensor;
+    private Sensor mRotationVectorSensor;
+    private Sensor mSignificantMotionSensor;
+    private Sensor mStepCounterSensor;
+    private Sensor mStepDetectorSensor;
+
     private long mLasttimestamp;
-    private Sensor mRotationSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,7 +86,17 @@ public class MainActivity extends Activity implements SensorEventListener {
         mGyroscopeSensor = mSensorManager.getDefaultSensor(SENS_GYROSCOPE);
         mGyroscopeUncalibratedSensor = mSensorManager.getDefaultSensor(SENS_GYROSCOPE_UNCALIBRATED);
         mHeartrateSensor = mSensorManager.getDefaultSensor(SENS_HEARTRATE);
-        mRotationSensor = mSensorManager.getDefaultSensor(SENS_ROTATION_VECTOR);
+        mLightSensor = mSensorManager.getDefaultSensor(SENS_LIGHT);
+        mLinearAccelerationSensor = mSensorManager.getDefaultSensor(SENS_LINEAR_ACCELERATION);
+        mMagneticFieldSensor = mSensorManager.getDefaultSensor(SENS_MAGNETIC_FIELD);
+        mMagneticFieldUncalibratedSensor = mSensorManager.getDefaultSensor(SENS_MAGNETIC_FIELD_UNCALIBRATED);
+        mPressureSensor = mSensorManager.getDefaultSensor(SENS_PRESSURE);
+        mProximitySensor = mSensorManager.getDefaultSensor(SENS_PROXIMITY);
+        mHumiditySensor = mSensorManager.getDefaultSensor(SENS_HUMIDITY);
+        mRotationVectorSensor = mSensorManager.getDefaultSensor(SENS_ROTATION_VECTOR);
+        mSignificantMotionSensor = mSensorManager.getDefaultSensor(SENS_SIGNIFICANT_MOTION);
+        mStepCounterSensor = mSensorManager.getDefaultSensor(SENS_STEP_COUNTER);
+        mStepDetectorSensor = mSensorManager.getDefaultSensor(SENS_STEP_DETECTOR);
     }
 
     @Override
@@ -84,60 +104,54 @@ public class MainActivity extends Activity implements SensorEventListener {
         super.onResume();
         // Register the listener
         if (mSensorManager != null) {
-//            if (mAccelerometerSensor != null) {
-//                mSensorManager.registerListener(this, mAccelerometerSensor, SensorManager.SENSOR_DELAY_FASTEST);
-//            } else {
-//                Log.d(TAG, "mAcc");
-//            }
+            if (mAccelerometerSensor != null) {
+                mSensorManager.registerListener(this, mAccelerometerSensor, SensorManager.SENSOR_DELAY_FASTEST);
+            } else {
+                Log.w(TAG, "No Accelerometer found");
+            }
 
-//            if (mAmbientTemperatureSensor != null) {
-//                mSensorManager.registerListener(this, mAmbientTemperatureSensor, SensorManager.SENSOR_DELAY_FASTEST);
-//            } else {
-//                Log.d(TAG, "mAmbient");
-//            }
+            if (mAmbientTemperatureSensor != null) {
+                mSensorManager.registerListener(this, mAmbientTemperatureSensor, SensorManager.SENSOR_DELAY_FASTEST);
+            } else {
+                Log.w(TAG, "Ambient Temperature Sensor not found");
+            }
 
             if (mGameRotationVectorSensor != null) {
                 mSensorManager.registerListener(this, mGameRotationVectorSensor, SensorManager.SENSOR_DELAY_FASTEST);
             } else {
-                Log.d(TAG, "mGaming");
-            }
-
-            if (mRotationSensor != null) {
-                mSensorManager.registerListener(this, mRotationSensor, SensorManager.SENSOR_DELAY_FASTEST);
-            } else {
-                Log.d(TAG, "mRotating");
+                Log.w(TAG, "Gaming Rotation Vector Sensor not found");
             }
 
             if (mGeomagneticSensor != null) {
                 mSensorManager.registerListener(this, mGeomagneticSensor, SensorManager.SENSOR_DELAY_FASTEST);
             } else {
-                Log.d(TAG, "mGeomag");
+                Log.w(TAG, "No Geomagnetic Sensor found");
             }
 
             if (mGravitySensor != null) {
                 mSensorManager.registerListener(this, mGravitySensor, SensorManager.SENSOR_DELAY_FASTEST);
             } else {
-                Log.d(TAG, "mGrav");
+                Log.w(TAG, "No Gravity Sensor");
             }
 
             if (mGyroscopeUncalibratedSensor != null) {
                 mSensorManager.registerListener(this, mGyroscopeUncalibratedSensor, SensorManager.SENSOR_DELAY_FASTEST);
             } else {
-                Log.d(TAG, "mGyr");
+                Log.w(TAG, "No Uncalibrated Gyroscope Sensor found");
             }
 
-//            if (mHeartrateSensor != null) {
-//                mSensorManager.registerListener(this, mHeartrateSensor, SensorManager.SENSOR_DELAY_FASTEST);
-//            } else {
-//                Log.d(TAG, "mHeart");
-//            }
+            if (mHeartrateSensor != null) {
+                mSensorManager.registerListener(this, mHeartrateSensor, SensorManager.SENSOR_DELAY_FASTEST);
+            } else {
+                Log.d(TAG, "No Heartrate Sensor found");
+            }
         }
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        //Unregister the listener
+
         if (mSensorManager != null)
             mSensorManager.unregisterListener(this);
     }

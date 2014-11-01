c1109231616bc75502c0412bb5e1b3bb0e3a1838
Sebastian Kaspari <sebastian.kaspari@quartett-mobile.de>
2014-11-01 16:24:50 +0000

    Communication layer.

---
+++ mobile/src/main/java/com/github/pocmo/sensordashboard/SensorReceiverService.java
@@ -0,0 +1,59 @@
+package com.github.pocmo.sensordashboard;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import com.github.pocmo.sensordashboard.shared.DataMapKeys;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataItem;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.Node;
+import com.google.android.gms.wearable.WearableListenerService;
+
+import java.util.Arrays;
+
+public class SensorReceiverService extends WearableListenerService {
+    private static final String TAG = "SensorDashboard/SensorReceiverService";
+
+    @Override
+    public void onPeerConnected(Node peer) {
+        super.onPeerConnected(peer);
+
+        Log.i(TAG, "Connected: " + peer.getDisplayName() + " (" + peer.getId() + ")");
+    }
+
+    @Override
+    public void onPeerDisconnected(Node peer) {
+        super.onPeerDisconnected(peer);
+
+        Log.i(TAG, "Disconnected: " + peer.getDisplayName() + " (" + peer.getId() + ")");
+    }
+
+    @Override
+    public void onDataChanged(DataEventBuffer dataEvents) {
+        Log.d(TAG, "onDataChanged()");
+
+        for (DataEvent dataEvent : dataEvents) {
+            if (dataEvent.getType() == DataEvent.TYPE_CHANGED) {
+                DataItem dataItem = dataEvent.getDataItem();
+
+                unpackSensorData(
+                    Integer.parseInt(dataItem.getUri().getLastPathSegment()),
+                    DataMapItem.fromDataItem(dataItem).getDataMap()
+                );
+            }
+        }
+    }
+
+    private void unpackSensorData(int sensorType, DataMap dataMap) {
+        int accuracy = dataMap.getInt(DataMapKeys.ACCURACY);
+        long timestamp = dataMap.getLong(DataMapKeys.TIMESTAMP);
+        float[] values = dataMap.getFloatArray(DataMapKeys.VALUES);
+
+        Log.d(TAG, "Received sensor data " + sensorType + " = " + Arrays.toString(values));
+
+        Toast.makeText(this, "Received sensor data " + sensorType + " = " + Arrays.toString(values), Toast.LENGTH_SHORT).show();
+    }
+}
--- wear/src/main/java/com/github/pocmo/sensordashboard/MainActivity.java
+++ wear/src/main/java/com/github/pocmo/sensordashboard/MainActivity.java
@@ -3,22 +3,26 @@ package com.github.pocmo.sensordashboard;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.wearable.view.WatchViewStub;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
-    private TextView mTextView;
+    private DeviceClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
-        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
-            @Override
-            public void onLayoutInflated(WatchViewStub stub) {
-                mTextView = (TextView) stub.findViewById(R.id.text);
-            }
-        });
+
+        client = DeviceClient.getInstance(this);
+    }
+
+    public void onBeep(View view) {
+        client.sendSensorData(
+            0,
+            1,
+            System.currentTimeMillis(),
+            new float[] { 1.0f }
+        );
     }
 }

fd2628a433e02f3b06e97c5a609e58f26c3a211d
Sebastian Kaspari <sebastian.kaspari@quartett-mobile.de>
2014-11-01 17:49:47 +0000

    Randomise dummy event data.

--- wear/src/main/java/com/github/pocmo/sensordashboard/MainActivity.java
+++ wear/src/main/java/com/github/pocmo/sensordashboard/MainActivity.java
@@ -6,23 +6,22 @@ import android.support.wearable.view.WatchViewStub;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends Activity {
     private DeviceClient client;
+    private Random random;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        random = new Random();
         client = DeviceClient.getInstance(this);
     }
 
     public void onBeep(View view) {
-        client.sendSensorData(
-            0,
-            1,
-            System.currentTimeMillis(),
-            new float[] { 1.0f }
-        );
+        client.sendSensorData(0, 1, System.currentTimeMillis(), new float[] { random.nextFloat() });
     }
 }
